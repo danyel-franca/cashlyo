fe849fb8ac668733b18ec274ecc41fdbec8c412a
Danyel França <danyelfranca.vasco@gmail.com>
2026-06-30 15:48:17 -0300

    feat: improves user experience

--- src/app/features/dashboard/components/header/header.ts
+++ src/app/features/dashboard/components/header/header.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +8,40 @@ import { Output, EventEmitter } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
+  constructor(private router: Router) { }
+
   @Output()
   menuClick = new EventEmitter<void>();
 
+  userName = 'Usuário';
+
+  userEmail = '';
+
+  ngOnInit(): void {
+    this.userName = localStorage.getItem('userName') || 'Usuário';
+
+    this.userEmail = localStorage.getItem('userEmail') || '';
+  }
+
   abrirMenu(): void {
-  this.menuClick.emit();
-}
+    this.menuClick.emit();
+  }
+
+  goToProfile(): void {
+    this.router.navigate(['/dashboard/profile']);
+  }
+
+  get initials(): string {
+    const names = this.userName.trim().split(' ');
+
+    if (names.length === 1) {
+      return names[0].charAt(0).toUpperCase();
+    }
+
+    return (
+      names[0].charAt(0) +
+      names[names.length - 1].charAt(0)
+    ).toUpperCase();
+  }
 }
